084941be652e92fd1eb0cab77757876b0127658d
infra_projects <rotero82@gmail.com>
2023-07-29 19:15:52 -0400

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,26 +1,57 @@
-# modules/vpc/main.tf
-resource "aws_vpc" "my_vpc" {
-  cidr_block = var.vpc_cidr_block
-  enable_dns_hostnames = true
-  enable_dns_support = true
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "Main VPC"
+  }
 }
 
 resource "aws_subnet" "public" {
-  count         = length(var.availability_zones)
-  cidr_block    = cidrsubnet(var.vpc_cidr_block, 8, count.index + 1)
-  availability_zone = element(var.availability_zones, count.index)
+  count = var.az_count * 2
+
+  vpc_id     = aws_vpc.main.id
+  cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
 
   tags = {
     Name = "Public Subnet ${count.index + 1}"
   }
 }
 
-resource "aws_subnet" "private" {
-  count         = length(var.availability_zones)
-  cidr_block    = cidrsubnet(var.vpc_cidr_block, 8, count.index + 11)
-  availability_zone = element(var.availability_zones, count.index)
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "Private Subnet ${count.index + 1}"
+    Name = "Internet Gateway"
   }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "Public Route Table"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count = var.az_count * 2
+
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public.id
+}
+
+data "aws_availability_zones" "available" {}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnets" {
+  value = aws_subnet.public.*.id
 }
