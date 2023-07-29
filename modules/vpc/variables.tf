1787ea2d4571367986f62b4e93d67691134d27ad
infra_projects <rotero82@gmail.com>
2023-07-29 19:16:23 -0400

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,3 +1,9 @@
-variable "vpc_cidr_block" {
-  type = string
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+}
+
+variable "az_count" {
+  description = "Number of availability zones"
+  type        = number
 }
