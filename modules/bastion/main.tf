093b26d2da1a33820f678129beb547181f549519
infra_projects <rotero82@gmail.com>
2023-07-29 18:40:40 -0400

    Update main.tf

--- modules/bastion/main.tf
+++ modules/bastion/main.tf
@@ -30,7 +30,3 @@ resource "aws_security_group" "http_ssh" {
 output "public_ip" {
   value = aws_instance.bastion.public_ip
 }
-
-output "security_group_id" {
-  value = aws_security_group.http_ssh.id
-}
