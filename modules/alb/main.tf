86c44dd99a92480f2ed795de083b21590921ac73
infra_projects <rotero82@gmail.com>
2023-07-30 13:49:09 -0400

    Update main.tf

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -16,7 +16,7 @@ resource "aws_alb" "alb" {
   name            = "my-alb"
   security_groups = [aws_security_group.alb_sg.id]
   subnets         = var.subnets
-
+  load_balancer_type = "application"
   tags = {
     Name = "ALB"
   }
