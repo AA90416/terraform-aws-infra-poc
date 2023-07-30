ddc5709830092e55503a238ff00afc5338f8f21c
infra_projects <rotero82@gmail.com>
2023-07-29 22:28:45 -0400

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,6 +1,5 @@
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
-
   tags = {
     Name = "Main VPC"
   }
@@ -8,12 +7,9 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "public" {
   count = var.az_count
-
   vpc_id     = aws_vpc.main.id
   cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index)
-
   availability_zone = element(data.aws_availability_zones.available.names, count.index)
-
   tags = {
     Name = "Public Subnet ${count.index + 1}"
   }
@@ -21,12 +17,9 @@ resource "aws_subnet" "public" {
 
 resource "aws_subnet" "private" {
   count = var.az_count
-
   vpc_id     = aws_vpc.main.id
   cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index + 4)  # Use count.index + 4 to avoid overlapping with public subnet CIDRs
-
   availability_zone = element(data.aws_availability_zones.available.names, count.index)
-
   tags = {
     Name = "Private Subnet ${count.index + 1}"
   }
@@ -34,7 +27,6 @@ resource "aws_subnet" "private" {
 
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
-
   tags = {
     Name = "Internet Gateway"
   }
@@ -42,12 +34,10 @@ resource "aws_internet_gateway" "gw" {
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
-
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
-
   tags = {
     Name = "Public Route Table"
   }
@@ -55,17 +45,14 @@ resource "aws_route_table" "public" {
 
 resource "aws_route_table_association" "public" {
   count = var.az_count
-
   subnet_id      = element(aws_subnet.public.*.id, count.index)
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_nat_gateway" "nat" {
   count = var.az_count
-
   allocation_id = aws_eip.nat[count.index].id
   subnet_id     = element(aws_subnet.public.*.id, count.index)
-
   tags = {
     Name = "NAT Gateway ${count.index + 1}"
   }
@@ -73,7 +60,6 @@ resource "aws_nat_gateway" "nat" {
 
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
-
   tags = {
     Name = "Private Route Table"
   }
