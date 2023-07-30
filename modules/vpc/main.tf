0790355b181a2dff05f7b168a86e251679d9dd35
infra_projects <rotero82@gmail.com>
2023-07-30 02:56:22 -0400

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,3 +1,7 @@
+# modules/vpc/main.tf
+
+data "aws_availability_zones" "available" {}
+
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
 
@@ -30,62 +34,4 @@ resource "aws_subnet" "private" {
   }
 }
 
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "Internet Gateway"
-  }
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
-  tags = {
-    Name = "Public Route Table"
-  }
-}
-
-resource "aws_route_table_association" "public" {
-  count = var.az_count
-
-  subnet_id      = element(aws_subnet.public.*.id, count.index)
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_nat_gateway" "nat" {
-  count = var.az_count
-
-  allocation_id = aws_eip.nat[count.index].id
-  subnet_id     = element(aws_subnet.public.*.id, count.index)
-
-  tags = {
-    Name = "NAT Gateway ${count.index + 1}"
-  }
-}
-
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "Private Route Table"
-  }
-}
-
-resource "aws_route" "nat_gateway" {
-  count = var.az_count
-  route_table_id         = aws_route_table.private.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat[count.index].id
-}
-
-resource "aws_route_table_association" "private" {
-  count = var.az_count
-  subnet_id      = element(aws_subnet.private.*.id, count.index)
-  route_table_id = aws_route_table.private.id
-}
+// Rest of the resources...
