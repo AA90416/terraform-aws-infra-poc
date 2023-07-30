46c0970b4fb9228a4a62b6b3f4bf32cf650a25be
infra_projects <rotero82@gmail.com>
2023-07-30 09:24:18 -0400

    Update variables.tf

--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -1,7 +1,3 @@
-variable "subnets" {
-  description = "List of subnet IDs where ALB will be created"
-  type        = list(string)
-}
 
 variable "security_group_ids" {
   description = "List of security group IDs attached to the ALB"
@@ -13,20 +9,6 @@ variable "vpc_id" {
   type        = string
 }
 
-variable "asg_instance_type" {
-  description = "Instance type for the Auto Scaling Group (ASG) instances"
-  type        = string
-}
-
-variable "asg_ami" {
-  description = "ID of the Amazon Machine Image (AMI) to be used for the ASG instances"
-  type        = string
-}
-
-variable "instance_count" {
-  description = "Number of instances to be created in the ASG"
-  type        = number
-}
 
 variable "subnet_id" {
   description = "ID of the subnet where the ASG instances will be launched"
