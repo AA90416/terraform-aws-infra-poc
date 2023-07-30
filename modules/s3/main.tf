ed64b95cb3f7f5c9b8b2844fe98f1cd946afd2ab
infra_projects <rotero82@gmail.com>
2023-07-30 10:18:13 -0400

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,12 +1,10 @@
 # modules/s3/main.tf
 
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket = var.bucket_name
-  acl    = "private"
-  lifecycle_rule {
+resource "aws_s3_bucket_lifecycle_configuration" "s3_bucket_lifecycle" {
+  rule {
     id      = "images_rule"
+    status  = "Enabled"
     prefix  = "Images/"
-    enabled = true  # Enable this lifecycle rule
 
     transition {
       days          = 90
@@ -14,16 +12,44 @@ resource "aws_s3_bucket" "s3_bucket" {
     }
   }
 
-  lifecycle_rule {
+  rule {
     id      = "logs_rule"
+    status  = "Enabled"
     prefix  = "Logs/"
-    enabled = true  # Enable this lifecycle rule
 
     expiration {
       days = 90
     }
   }
+
+  bucket = var.bucket_name
 }
+
+
+#resource "aws_s3_bucket" "s3_bucket" {
+#  bucket = var.bucket_name
+#  acl    = "private"
+#  lifecycle_rule {
+#    id      = "images_rule"
+#    prefix  = "Images/"
+#    enabled = true  # Enable this lifecycle rule
+#
+#    transition {
+#      days          = 90
+#      storage_class = "GLACIER"
+#    }
+#  }
+
+#  lifecycle_rule {
+#    id      = "logs_rule"
+#    prefix  = "Logs/"
+#    enabled = true  # Enable this lifecycle rule
+#
+#    expiration {
+#      days = 90
+#    }
+#  }
+#}
 
 
 terraform {
