6ef31cc3d57332165de6aee6c30375b75e36dd96
infra_projects <rotero82@gmail.com>
2023-07-29 21:22:04 -0400

    Update main.tf

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,33 +1,47 @@
-resource "aws_alb" "alb" {
-  name               = "my-alb"
-  subnets            = var.subnets
-  security_groups    = var.security_group_ids
+resource "aws_security_group" "alb_sg" {
+  name_prefix = "alb-sg-"
+
+  # Security group rules for allowing traffic on port 80 (HTTP)
+  ingress {
+    description = "HTTP Access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    # Allow traffic from specific IPs or ranges as needed
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-resource "aws_alb_listener" "http_listener" {
-  load_balancer_arn = aws_alb.alb.arn
-  port              = 80
-  protocol          = "HTTP"
+resource "aws_alb" "alb" {
+  name            = "my-alb"
+  security_groups = [aws_security_group.alb_sg.id]
+  subnets         = var.subnets
 
-  default_action {
-    type = "forward"
-    target_group_arn = aws_alb_target_group.asg_target_group.arn
+  tags = {
+    Name = "ALB"
   }
 }
 
 resource "aws_alb_target_group" "asg_target_group" {
-  name     = "my-target-group"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpc.selected.id
+  name        = "asg-target-group"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "instance"
 }
 
 resource "aws_alb_target_group_attachment" "asg_attachment" {
   target_group_arn = aws_alb_target_group.asg_target_group.arn
   target_id        = aws_instance.asg.*.id
-  port             = 80
 }
 
-data "aws_vpc" "selected" {
-  default = true
+resource "aws_alb_listener" "alb_listener" {
+  load_balancer_arn = aws_alb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_alb_target_group.asg_target_group.arn
+    type             = "forward"
+  }
 }
