6598ee81310a49155989e77ad8d5151f4d407758
infra_projects <rotero82@gmail.com>
2023-07-30 02:22:49 -0400

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,25 +1,52 @@
+variable "lifecycle_rules" {
+  type = list(map(any))
+  default = [
+    {
+      id      = "images_rule"
+      prefix  = "Images/"
+      status  = "Enabled"
+
+      transition = {
+        days          = 90
+        storage_class = "GLACIER"
+      }
+    },
+    {
+      id      = "logs_rule"
+      prefix  = "Logs/"
+      status  = "Enabled"
+
+      expiration = {
+        days = 90
+      }
+    }
+  ]
+}
+
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
   acl    = "private"
-  lifecycle_rule {
-    id      = "images_rule"
-    prefix  = "Images/"
-    status  = "Enabled"
 
-    transition {
-      days          = 90
-      storage_class = "GLACIER"
+  for_each = { for idx, rule in var.lifecycle_rules : idx => rule }
+
+  lifecycle_rule {
+    id     = each.value.id
+    prefix = each.value.prefix
+    status = each.value.status
+
+    dynamic "transition" {
+      for_each = can(each.value.transition) ? [1] : []
+      content {
+        days          = each.value.transition.days
+        storage_class = each.value.transition.storage_class
+      }
     }
-  }
 
-  lifecycle_rule {
-    id      = "logs_rule"
-    prefix  = "Logs/"
-    status  = "Enabled"
-
-    expiration {
-      days = 90
+    dynamic "expiration" {
+      for_each = can(each.value.expiration) ? [1] : []
+      content {
+        days = each.value.expiration.days
+      }
     }
   }
 }
-
