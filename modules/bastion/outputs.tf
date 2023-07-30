9a48252cda8c75a6d614a81d2738e883e28e5875
infra_projects <rotero82@gmail.com>
2023-07-30 11:18:49 -0400

    Update outputs.tf

--- modules/bastion/outputs.tf
+++ modules/bastion/outputs.tf
@@ -3,9 +3,9 @@ output "bastion_public_ip" {
   value       = aws_instance.bastion.public_ip
 }
 
-output "security_group_id" {
-  value = aws_security_group.http_ssh.id
-}
+#output "security_group_id" {
+#  value = aws_security_group.http_ssh.id
+#}
 
 output "security_group_id" {
   value = aws_security_group.bastion.id
