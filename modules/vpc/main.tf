4b0859a046fe03d8a2475900a17d1fb698fe3127
infra_projects <rotero82@gmail.com>
2023-07-30 03:00:49 -0400

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -62,4 +62,4 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
-data "aws_availability_zones" "available" {}
+#data "aws_availability_zones" "available" {}
