6e6b2e0e2ee5aea877935bfe39eae970c9229cd5
infra_projects <rotero82@gmail.com>
2023-07-29 23:24:06 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,6 +4,13 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
+variable "s3_lifecycle_rules" {
+  description = "List of S3 bucket lifecycle rules"
+  type        = list(map(any))
+  default     = []
+}
+
+
 variable "vpc_cidr" {
   description = "CIDR block for VPC"
   type        = string
