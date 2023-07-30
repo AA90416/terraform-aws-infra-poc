59cb4feaeeb5cd6d4b0cbf5e049f82b80414b5a0
infra_projects <rotero82@gmail.com>
2023-07-29 21:02:22 -0400

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,14 +1,11 @@
 output "vpc_id" {
-  description = "The ID of the VPC."
-  value       = aws_vpc.my_vpc.id
+  value = aws_vpc.main.id
 }
 
 output "public_subnets" {
-  description = "List of IDs of the public subnets."
-  value       = aws_subnet.public.*.id
+  value = aws_subnet.public.*.id
 }
 
 output "private_subnets" {
-  description = "List of IDs of the private subnets."
-  value       = aws_subnet.private.*.id
+  value = aws_subnet.private.*.id
 }
