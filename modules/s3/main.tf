8ed1f25a56fdcc20c806834a44ded3e4c03142cf
infra_projects <rotero82@gmail.com>
2023-07-30 02:49:11 -0400

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -2,30 +2,25 @@ resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
   acl    = "private"
 
-  dynamic "lifecycle_rule" {
-    for_each = var.lifecycle_rules
-    content {
-      id      = lifecycle_rule.value.id
-      prefix  = lifecycle_rule.value.prefix
+  # Lifecycle rules for S3 bucket
+  lifecycle_rule {
+    id      = "images_rule"
+    prefix  = "Images/"
+    status  = "Enabled"
 
-      dynamic "transition" {
-        for_each = can(lifecycle_rule.value.transition) ? [lifecycle_rule.value.transition] : []
-        content {
-          days          = transition.value.days
-          storage_class = transition.value.storage_class
-        }
-      }
-
-      dynamic "expiration" {
-        for_each = can(lifecycle_rule.value.expiration) ? [lifecycle_rule.value.expiration] : []
-        content {
-          days = expiration.value.days
-        }
-      }
+    transition {
+      days          = 90
+      storage_class = "GLACIER"
     }
   }
-}
 
-terraform {
-  experiments = [module_variable_optional_attrs]
+  lifecycle_rule {
+    id      = "logs_rule"
+    prefix  = "Logs/"
+    status  = "Enabled"
+
+    expiration {
+      days = 90
+    }
+  }
 }
