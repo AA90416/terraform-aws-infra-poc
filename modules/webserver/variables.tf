df44c248256baf7edeb49ecf9a0b48412a952e6a
infra_projects <rotero82@gmail.com>
2023-07-29 22:23:19 -0400

    Update variables.tf

--- modules/webserver/variables.tf
+++ modules/webserver/variables.tf
@@ -1,11 +1,29 @@
+variable "subnets" {
+  description = "List of subnet IDs where webserver instances will be launched"
+  type        = list(string)
+}
+
+variable "security_group_ids" {
+  description = "List of security group IDs attached to the webserver instances"
+  type        = list(string)
+}
+
 variable "webserver_ami" {
-  type = string
+  description = "The AMI ID for the webserver instances"
+  type        = string
 }
 
 variable "webserver_instance_type" {
-  type = string
+  description = "Instance type for webserver instances"
+  type        = string
 }
 
 variable "webserver_key_pair" {
-  type = string
+  description = "Name of the AWS Key Pair to be used for instances in the ASG"
+  type        = string
+}
+
+variable "webserver_instance_count" {
+  description = "Number of webserver instances in the Auto Scaling Group"
+  type        = number
 }
