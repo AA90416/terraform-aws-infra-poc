08e254cdb90e7b42ebdee754bbe4f5064d24f2c7
infra_projects <rotero82@gmail.com>
2023-07-30 19:34:10 -0400

    Update variables.tf

--- modules/asg/variables.tf
+++ modules/asg/variables.tf
@@ -52,7 +52,8 @@ variable "instance_count" {
 #}
 
 variable "allowed_ssh_ip" {
-  description = "List your public IP address or range to allow SSH"
-  type        = list(string)
-  default     = ["your_public_ip_address_or_range"]
+  description = "Single CIDR block to allow SSH access"
+  type        = string
+  default     = "203.0.113.0/24" # Replace with your actual CIDR block
 }
+
