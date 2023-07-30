4e21219bdbddf6409ba834a197822517ec785c84
infra_projects <rotero82@gmail.com>
2023-07-30 02:13:56 -0400

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -2,24 +2,3 @@ resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
   acl    = "private"
 
-  lifecycle_rule {
-    id      = "images_rule"
-    prefix  = "Images/"
-    status  = "Enabled"
-
-    transition {
-      days          = 90
-      storage_class = "GLACIER"
-    }
-  }
-
-  lifecycle_rule {
-    id      = "logs_rule"
-    prefix  = "Logs/"
-    status  = "Enabled"
-
-    expiration {
-      days = 90
-    }
-  }
-}
