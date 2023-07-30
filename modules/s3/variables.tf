0a9e6ab197c37678467a3f620dab0ee74a604739
infra_projects <rotero82@gmail.com>
2023-07-30 02:27:46 -0400

    Update variables.tf

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -3,3 +3,61 @@ variable "bucket_name" {
   type        = string
 }
 
+variable "lifecycle_rules" {
+  type = list(map(any))
+  default = [
+    {
+      id      = "images_rule"
+      prefix  = "Images/"
+      status  = "Enabled"
+      transition = {
+        days          = 90
+        storage_class = "GLACIER"
+      }
+      expiration = {}
+    },
+    {
+      id      = "logs_rule"
+      prefix  = "Logs/"
+      status  = "Enabled"
+      transition = {}
+      expiration = {
+        days = 90
+      }
+    }
+  ]
+}
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = var.bucket_name
+  acl    = "private"
+
+  for_each = { for idx, rule in var.lifecycle_rules : idx => rule }
+
+  lifecycle_rule {
+    id     = each.value.id
+    prefix = each.value.prefix
+    status = each.value.status
+
+    dynamic "transition" {
+      for_each = can(each.value.transition) ? [each.value.transition] : []
+      content {
+        days          = transition.value.days
+        storage_class = transition.value.storage_class
+      }
+    }
+
+    dynamic "expiration" {
+      for_each = can(each.value.expiration) ? [each.value.expiration] : []
+      content {
+        days = expiration.value.days
+      }
+    }
+  }
+}
+Now the default value for the "lifecycle_rules" variable should be compatible with the list of maps type, and the error should be resolved.
+
+
+
+
+
