1f842e5d6fa438ee8c8cd50b94456aa837050bce
infra_projects <rotero82@gmail.com>
2023-07-29 22:18:35 -0400

    Update outputs.tf

--- modules/bastion/outputs.tf
+++ modules/bastion/outputs.tf
@@ -2,3 +2,7 @@ output "bastion_public_ip" {
   description = "Public IP address of the bastion host."
   value       = aws_instance.bastion.public_ip
 }
+
+output "security_group_id" {
+  value = aws_security_group.http_ssh.id
+}
