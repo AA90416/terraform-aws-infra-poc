76650e1ed405e056c725c7ade23d097ae308079b
infra_projects <rotero82@gmail.com>
2023-07-30 02:28:58 -0400

    Update variables.tf

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -28,34 +28,5 @@ variable "lifecycle_rules" {
   ]
 }
 
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket = var.bucket_name
-  acl    = "private"
-
-  for_each = { for idx, rule in var.lifecycle_rules : idx => rule }
-
-  lifecycle_rule {
-    id     = each.value.id
-    prefix = each.value.prefix
-    status = each.value.status
-
-    dynamic "transition" {
-      for_each = can(each.value.transition) ? [each.value.transition] : []
-      content {
-        days          = transition.value.days
-        storage_class = transition.value.storage_class
-      }
-    }
-
-    dynamic "expiration" {
-      for_each = can(each.value.expiration) ? [each.value.expiration] : []
-      content {
-        days = expiration.value.days
-      }
-    }
-  }
-}
-
-
 
 
