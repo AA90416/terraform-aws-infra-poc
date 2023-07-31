2e3c06dde215cc3bccc2af3e7dd52b24d3ebd89b
infra_projects <rotero82@gmail.com>
2023-07-31 11:08:49 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -253,7 +253,7 @@ resource "aws_launch_configuration" "webserver-launch-config" {
   
   root_block_device {
             volume_type = "gp2"
-            volume_size = 10
+            volume_size = 20
             encrypted   = true
         }
 
@@ -276,8 +276,8 @@ resource "aws_launch_configuration" "webserver-launch-config" {
 resource "aws_autoscaling_group" "Demo-ASG-tf" {
   name		     = "Demo-ASG-tf"
   desired_capacity   = 1
-  max_size           = 2
-  min_size           = 1
+  max_size           = 6
+  min_size           = 2
   force_delete       = true
   depends_on 	     = [aws_lb.ALB-tf]
   target_group_arns  =  [aws_lb_target_group.TG-tf.arn]
