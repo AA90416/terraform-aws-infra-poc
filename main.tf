7ae7c67e26586fbef7646ebae1a6804fc1944a98
gitadmin <60225190+SNET-ARCH@users.noreply.github.com>
2023-07-31 09:57:13 -0400

    Removed warnings

--- main.tf
+++ main.tf
@@ -280,7 +280,7 @@ resource "aws_autoscaling_group" "Demo-ASG-tf" {
   min_size           = 1
   force_delete       = true
   depends_on 	     = [aws_lb.ALB-tf]
-  target_group_arns  =  ["${aws_lb_target_group.TG-tf.arn}"]
+  target_group_arns  =  [aws_lb_target_group.TG-tf.arn]
   health_check_type  = "EC2"
   launch_configuration = aws_launch_configuration.webserver-launch-config.name
   vpc_zone_identifier = ["${aws_subnet.prv_sub1.id}","${aws_subnet.prv_sub2.id}"]
