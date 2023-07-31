d7aecff581f9ce2ec0ea4cd58e50ec78cf78e7a6
infra_projects <rotero82@gmail.com>
2023-07-30 20:40:58 -0400

    Update main.tf

--- modules/asg/main.tf
+++ modules/asg/main.tf
@@ -34,7 +34,7 @@ resource "aws_launch_configuration" "asg_lc" {
 
 resource "aws_autoscaling_attachment" "alb_attachment" {
   autoscaling_group_name = aws_autoscaling_group.asg.name
-  alb_target_group_arn   = aws_alb_target_group.asg_target_group.arn
+  target_group_arn       = aws_alb_target_group.asg_target_group.arn
 }
 
 
