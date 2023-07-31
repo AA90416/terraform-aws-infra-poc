65edf326b8f94696750c572a2c201eb6db6d7773
infra_projects <rotero82@gmail.com>
2023-07-31 14:30:49 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -5,9 +5,5 @@ terraform {
     bucket         = "your-terraform-state-bucket"  # Replace with your unique S3 bucket name for storing Terraform state
     key            = "terraform.tfstate"           # Replace with a desired name for the Terraform state file
     region         = "us-east-1"                   # Replace with your desired AWS region
-#    dynamodb_table = "terraform-lock"              # Replace with your desired name for the DynamoDB table (for state locking)
-
-    # Optionally, you can use a profile name if you're using AWS CLI profiles
-    # profile        = "your-aws-profile"
   }
 }
