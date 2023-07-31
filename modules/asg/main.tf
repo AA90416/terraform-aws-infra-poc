326078d416177c7ca7929926dd2bea5e26387bb9
infra_projects <rotero82@gmail.com>
2023-07-30 20:52:54 -0400

    Update main.tf

--- modules/asg/main.tf
+++ modules/asg/main.tf
@@ -32,10 +32,7 @@ resource "aws_launch_configuration" "asg_lc" {
   security_groups = [aws_security_group.asg_sg.id] # Associate the security group with the instances
 }
 
-resource "aws_autoscaling_attachment" "alb_attachment" {
-  autoscaling_group_name = aws_autoscaling_group.asg.name
-  target_group_arns = [aws_alb_target_group.asg_target_group.arn]
-}
+
 
 
   #user_data = <<-EOF
