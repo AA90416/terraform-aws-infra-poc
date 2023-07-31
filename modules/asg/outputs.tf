68c6925d7293dfbac2fa19b30ca8453a66127ff7
infra_projects <rotero82@gmail.com>
2023-07-30 21:27:29 -0400

    Update outputs.tf

--- modules/asg/outputs.tf
+++ modules/asg/outputs.tf
@@ -9,28 +9,3 @@ output "instance_count" {
 output "autoscaling_group_name" {
   value = aws_autoscaling_group.asg.name
 }
-
-
-#output "asg_instance_ids" {
-#  value = aws_autoscaling_group.my_asg.*.instances[*].id
-#}
-#output "instance_type" {
-#  description = "The instance type used for the Auto Scaling Group."
-#  value       = aws_launch_configuration.asg_lc.instance_type
-#}
-
-#output "ami" {
-#  description = "The AMI ID used for the Auto Scaling Group instances."
-#  value       = aws_launch_configuration.asg_lc.image_id
-#}
-
-#output "instance_count" {
-#  description = "The number of instances in the Auto Scaling Group."
-#  value       = length(aws_autoscaling_group.asg)
-#}
-
-
-#output "subnet_ids" {
-#  description = "The list of subnet IDs where Auto Scaling Group instances are deployed."
-#h  value       = aws_autoscaling_group.asg.vpc_zone_identifier
-#}
