e9c25c2731673e04c06c39232d66b2148f9aa658
gitadmin <60225190+SNET-ARCH@users.noreply.github.com>
2023-07-31 11:10:33 -0400

    Removed unused folders

--- modules/vpc/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "vpc_cidr" {
-  description = "CIDR block for VPC"
-  type        = string
-}
-
-variable "az_count" {
-  description = "Number of availability zones"
-  type        = number
-}
